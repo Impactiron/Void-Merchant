23ad46f2c2b8586dad4ae5e58439362fb615769f
Impactiron <rothmich84@gmail.com>
2025-12-26 23:55:18 +0100

    Create EventsCenter.js

---
+++ js/core/EventsCenter.js
@@ -0,0 +1,12 @@
+// FILE: js/core/EventsCenter.js
+
+/**
+ * 📘 PROJECT: VOID MERCHANT
+ * MODULE: EVENTS CENTER
+ * * Zentraler Event-Bus für die Kommunikation zwischen losgelösten Systemen.
+ * * Singleton Pattern.
+ */
+
+const events = new Phaser.Events.EventEmitter();
+
+export default events;
